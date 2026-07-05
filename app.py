96c42f644c113f335be5e569551f388b05718717
HtetNaing <n3tby@unb.ca>
2026-07-05 18:43:36 -0300

    Adjusted for deployment

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from utilities.functions import execute
 #################### Web Server and Display ####################
 
 # Web-Page running
-app = Flask(__name__)
+app = Flask(__name__, static_folder="public", static_url_path="")
 
 @app.route("/")
 def home():
